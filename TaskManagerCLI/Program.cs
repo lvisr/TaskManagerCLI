b67b1b2424757da4b40e6bf96611f6ed4be7a906
lvisr <lvis@mail.com>
2025-02-09 13:34:14 +0000

    Added Delete and Mark Complete functionality

--- TaskManagerCLI/Program.cs
+++ TaskManagerCLI/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel.Design;
+using System.Data.Common;
 using System.IO;
 using Newtonsoft.Json;
 
@@ -6,13 +8,12 @@ public class Task
 {
     public int Id { get; set; }
     public string? Description { get; set; }
-    //public bool Complete { get; set; }
-    /*
-    ID
-    date added
-    description
-    status - complete/pending // bool complete false
-    */
+    public bool Complete { get; set; }
+
+    public Task()
+    {
+        Complete = false;
+    }
 }
 
 class Program 
@@ -26,13 +27,12 @@ class Program
         bool exitProgram = false;
         string option;
 
-        // read file to List<Task> Tasks
+        // read JSON file to List<Task> Tasks
         LoadTasks();
 
         while(!exitProgram)
         {
             DisplayMenu();
-            
             option = GetMenuInput();
 
             switch (option)
@@ -41,11 +41,26 @@ class Program
                     case "add":
                         AddTask();
                         SaveTasks();
-                        Console.WriteLine($"");
                         break;                    
                     case "v":
                     case "view":
                         DisplayTasks();
+                        Console.WriteLine("\nPress enter to continue");
+                        Console.ReadLine();
+                        break;
+                    case "d":
+                    case "delete":                        
+                        DisplayTasks();
+                        Console.WriteLine("\nSelect Task to Delete");
+                        DeleteTask(SelectTask());
+                        SaveTasks();
+                        break;
+                    case "m":
+                    case "mark":
+                        DisplayTasks();
+                        Console.WriteLine("\nSelect Task to Mark as Complete");
+                        MarkTaskComplete(SelectTask());
+                        SaveTasks();
                         break;
                     case "0":
                     case "exit":
@@ -83,7 +98,6 @@ class Program
         String? input;
         do
         {
-            Console.Write("\n ");
             input = Console.ReadLine();
         } while (input is null);
 
@@ -93,7 +107,6 @@ class Program
     {
         Task taskToAdd = new Task();
         
-
         Console.WriteLine("New task description: ");
         taskToAdd.Description = GetStringInput();
 
@@ -103,7 +116,6 @@ class Program
 
         Console.WriteLine("\nPress enter to continue");
         Console.ReadLine();
-
     }
     static void SaveTasks()
     {
@@ -116,14 +128,66 @@ class Program
         else 
             Console.WriteLine("There are no saved tasks");
     }
-    static void DisplayTasks()
+    static void DisplayTasks(int taskId = 0)
     {
-        foreach (Task currentTask in Tasks)
+        if (taskId == 0) // Display all tasks
         {
-            Console.WriteLine($"\nID: {currentTask.Id}\nDescription: {currentTask.Description}");
+            Console.WriteLine("\n*****    ALL TASKS    *****");
+            Console.WriteLine("\nTask #\tDescription:\n");
+            foreach (Task currentTask in Tasks)
+            {   
+                if (currentTask.Complete)
+                    Console.WriteLine($"   {currentTask.Id} \u2713\t{currentTask.Description}");
+                else
+                    Console.WriteLine($"   {currentTask.Id}\t{currentTask.Description}");
+            }
+            Console.WriteLine("\nComplete = \u2713");
+        }
+        else  // Display task with id = taskId
+        {
+            Console.WriteLine($"Task #{taskId}\t{Tasks.First(Task => Task.Id == taskId).Description}");
+        }
+
+    }
+    static int SelectTask()
+    {
+        int taskId;
+        string input;
+        Console.WriteLine("\nPlease insert a valid task #");
+        do
+        {
+            input = GetStringInput();
+        } while (!(int.TryParse(input, out taskId) && 1 <= taskId && taskId <= Tasks.Count));
+
+        return taskId;
+    }
+    static void DeleteTask(int idToDelete)
+    {
+        Console.WriteLine($"\nDeleted:");
+        DisplayTasks(idToDelete);
+
+        Tasks = Tasks.Where(Task => Task.Id != idToDelete).ToList();
+
+        foreach (Task item in Tasks)
+        {
+            if (item.Id >= idToDelete)
+                item.Id--;
         }
         Console.WriteLine("\nPress enter to continue");
         Console.ReadLine();
     }
-
+    static void MarkTaskComplete(int idToMark)
+    {
+        foreach (Task item in Tasks)
+        {
+            if (item.Id == idToMark)
+            {
+                item.Complete = true;
+                Console.WriteLine("\nSuccessfully Completed \u2713");
+                DisplayTasks(idToMark);                
+            }
+        }
+        Console.WriteLine("\nPress enter to continue");
+        Console.ReadLine();
+    }
 }
